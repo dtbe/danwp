778578ceddd3d77cc81bcb077a3002e7d3c0a3cc
dan <44970412+dtbe@users.noreply.github.com>
2023-06-24 22:46:51 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 
 
-$('#content').html(`this content comes from content.js 🤯`);
+$('#content').html(`
+  this content comes from content.js 🤯
+  <div class="h4 my-2">ideas:</div>
+  <ul>
+    <li>last/previously viewed, view history</li>
+  </ul>
+`);
 
 
