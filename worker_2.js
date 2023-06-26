ad2411fec0d39df08ad2e3f28f8c683610aec9dc
dan <44970412+dtbe@users.noreply.github.com>
2023-06-26 08:48:00 +0100

    Create worker_2.js

---
+++ worker_2.js
@@ -0,0 +1,27 @@
+
+
+
+
+function worker(l,b,c) {
+    const logs = l;
+    const background = b;
+    const color = c;
+
+    $(function() {
+
+        $('#workplace').html(`
+            <div class='text-highlight fw-bold'>live, laugh, love</div>
+            <div class='small'>the worker.js controls this bit, and it doesn't need deployment</div>
+            <div class='small'>it doesn't live here, it just works here</div>
+            <div class='small'>the css file isnt here either</div>
+            
+        `);
+
+        if (logs){
+            console.log("%c live, laugh, love ", `color: ${color}; background: ${background}`)
+        }
+
+        $.getScript("content.js");
+        
+    });
+}
